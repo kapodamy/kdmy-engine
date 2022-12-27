7410bf5467994de3d1d6595cd99ffe32f5c561af
kapodamy <16857233+kapodamy@users.noreply.github.com>
2022-12-27 16:45:56 -0300

    bump version to v0.48.9

--- src-desktop/kdy-e/engine/game/main.cs
+++ src-desktop/kdy-e/engine/game/main.cs
@@ -13,7 +13,7 @@ namespace Engine.Game {
 
         // this file contains all shared data across the game
         public const string ENGINE_NAME = "kdmy-engine";
-        public const string ENGINE_VERSION = "0.48.7";
+        public const string ENGINE_VERSION = "0.48.9";
 
         /**
          * The background music used in all menus, inherited from introscreen
