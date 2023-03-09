1a38baf4023e05ee05a1d5af6e72809be8cb4b6b
kapodamy <16857233+kapodamy@users.noreply.github.com>
2023-03-08 23:34:47 -0300

    v0.55 * bump engine version. * update readmes * from previous commits:     * Freeplay: allow save "Personal Best" of each song.     * FunkinSave: bump with v2 (includes fixes and new entries).     * Week: Improve voice mute to match Funkin behavior.     * Implement master volume (C# only) like in HaxeFlixel. Uses numpad +/- buttons (master mute is not implemented)     * Update all weeks "about.json" to include a display name.     * Updated luascript docs.

--- src-desktop/kdy-e/Properties/AssemblyInfo.cs
+++ src-desktop/kdy-e/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.53.0.0")]
-[assembly: AssemblyFileVersion("0.53.0.0")]
+[assembly: AssemblyVersion("0.55.0.0")]
+[assembly: AssemblyFileVersion("0.55.0.0")]
 [assembly: NeutralResourcesLanguage("es-419")]
